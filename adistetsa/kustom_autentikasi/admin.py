c589ec1d88e7ffa2c8ba1efa02d57c5a9c2f2272
arrazy100 <afdhalarrazy111@gmail.com>
2021-12-28 23:57:05 +0700

    Filter NISN Data Siswa User

--- adistetsa/kustom_autentikasi/admin.py
+++ adistetsa/kustom_autentikasi/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(DataSiswaUser)
+class DataSiswaUserAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['NISN', ]
+
+admin.site.register(DataSiswaUser, DataSiswaUserAdmin)
--- adistetsa/kustom_autentikasi/models.py
+++ adistetsa/kustom_autentikasi/models.py
@@ -7,3 +7,9 @@ from dataprofil.models import DataSiswa
 class DataSiswaUser(models.Model):
     USER = models.OneToOneField(User, on_delete=models.CASCADE)
     NISN = models.ForeignKey(DataSiswa, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.NISN)
+
+    class Meta:
+        verbose_name_plural = "Data Siswa User"
