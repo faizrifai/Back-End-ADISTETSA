27f86a92bf1b0aba0f00359ea2426d5ea4a58290
KMHDR21 <kevinmhdr123@gmail.com>
2021-12-30 14:41:39 +0700

    api beasiswa dan anak

--- adistetsa/dataprofil/serializers.py
+++ adistetsa/dataprofil/serializers.py
@@ -20,3 +20,36 @@ class DataGuruSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataGuru
         fields = '__all__'
+
+class DataKompetensiGuruSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataKompetensiGuru
+        fields = '__all__'
+
+class DataKompetensiKaryawanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataKompetensiKaryawan
+        fields = '__all__'
+
+
+
+class DataAnakGuruSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataAnakGuru
+        fields = '__all__'
+
+class DataAnakKaryawanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataAnakKaryawan
+        fields = '__all__'
+
+
+class DataBeasiswaGuruSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataBeasiswaGuru
+        fields = '__all__'
+
+class DataBeasiswaKaryawanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataBeasiswaKaryawan
+        fields = '__all__'
