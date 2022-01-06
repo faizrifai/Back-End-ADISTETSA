bf42829915aab42d6f01fd6f27c749426f1feb4a
Arzelf <firmansyahdecade919@gmail.com>
2022-01-06 15:11:57 +0700

    Nambah Aplikasi Kurikulum

--- adistetsa/adistetsa/urls.py
+++ adistetsa/adistetsa/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path, re_path
 
 from rest_framework import permissions
@@ -30,4 +32,4 @@ urlpatterns = [
     path('', include('kustom_autentikasi.urls')),
     path('', include('dataprofil.urls')),
     path('admin/', admin.site.urls, name='admin'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
