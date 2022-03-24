74cae32cc7ed0d281cf9d64a37c0af953f97625e
Arzelf <firmansyahdecade919@gmail.com>
2022-03-24 12:02:09 +0700

    keuangan

--- adistetsa/keuangan/models.py
+++ adistetsa/keuangan/models.py
@@ -2,6 +2,7 @@ from ast import Str
 from operator import mod
 from django.db import models
 from django.db.models.signals import post_save, pre_save
+from kurikulum.enums import ENUM_BULAN
 from dataprofil.models import DataSiswa
 from kurikulum.models import KelasSiswa
 from django.utils.text import Truncator
@@ -21,4 +22,12 @@ class Pembayaran(models.Model):
     ID = models.BigAutoField(primary_key=True)
     NAMA_SISWA = models.ForeignKey(KelasSiswa, on_delete=models.CASCADE)
     JENIS_PEMBAYARAN = models.ForeignKey(JenisPembayaran, on_delete=models.CASCADE)
+    PEMBAYARAN_BULAN = models.CharField(
+        max_length=255, 
+        choices=ENUM_BULAN,
+    )
+    TANGGAL_PEMBAYARAN = models.DateField()
+    NOMINAL_PEMBAYARAN = models.PositiveIntegerField()
+    
+    
     
