77c7c8053fec3c7a31f2dd3100af586b81d353d1
blacidtt <maftuhpistol@gmail.com>
2022-03-25 13:17:00 +0700

    adiwiyata baru

--- adistetsa/adiwiyata/admin.py
+++ adistetsa/adiwiyata/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.db.models import Sum
 
 from .forms import ReuseReduceRecycleForm, SanitasiDrainaseForm
 
 from.models import *
+from kurikulum.enums import ENUM_BULAN
 
 # Register your models here.
 class SanitasiDraineseAdmin (admin.ModelAdmin):
@@ -28,9 +30,22 @@ class PublikasiAdmin(admin.ModelAdmin):
 admin.site.register(Publikasi, PublikasiAdmin)
 
 class DaftarKaderAdmin(admin.ModelAdmin):
-    # search_fields = ('TANGGAL', 'NAMA_KEGIATAN', 'JENIS_KEGIATAN', 'KETERANGAN')
-    list_display = ('NIS',)
-    # list_per_page = 10
+    search_fields = ('NIS__NIS','NIS__NAMA', 'NIS__HP', 'NIS__ALAMAT')
+    list_display = ('nis','nama','handphone','alamat')
+    autocomplete_fields=('NIS',)
+    def nis (self, obj):
+        return obj.NIS.NIS
+    
+    def nama (self, obj):
+        return obj.NIS.NAMA
+    
+    def handphone(self, obj):
+        return obj.NIS.HP
+    
+    def alamat(self,obj):
+        return obj.NIS.ALAMAT
+    
+    list_per_page = 10
 
 admin.site.register(DaftarKader, DaftarKaderAdmin)
 
@@ -99,8 +114,61 @@ class PemeliharaanSampahAdmin(admin.ModelAdmin):
 admin.site.register(PemeliharaanSampah, PemeliharaanSampahAdmin)
 
 class TabunganSampahAdmin(admin.ModelAdmin):
-    search_fields = ('TANGGAL', 'JUMLAH_PERTANGGAL')
-    list_display = ('TANGGAL', 'JUMLAH_PERTANGGAL')
+    search_fields = ('TANGGAL', 'KATEGORI','JUMLAH')
+    list_display = ('TANGGAL', 'KATEGORI','JUMLAH')
     list_per_page = 10
 
 admin.site.register(TabunganSampah, TabunganSampahAdmin)
+
+class TabunganSampahProxyAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/adiwiyata/tabungansampahproxy_change_list.html'
+    date_hierarchy = 'TANGGAL'
+    
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context,
+        )
+
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+        
+        tanggal = None
+        
+        if response.context_data['preserved_filters']:
+            qp = response.context_data['preserved_filters'].split('=')[1]
+            dan = '%26'
+            equal = '%3D'
+            splitted = qp.split(dan)
+            for i in range(len(splitted)):
+                splitted[i] = splitted[i].split(equal)[-1]
+            
+            if len(splitted) >= 2:
+                bulan = int(splitted[-2]) - 1
+                bulan = ENUM_BULAN[bulan][0]
+                splitted[-2] = str(bulan)
+                
+            tanggal = " ".join(splitted)
+        else:
+            tanggal = 'Total Keseluruhan'
+        
+        print (qs)
+        
+        basah = 0
+        kering = 0
+        total = 0
+        for data in qs:
+            if data.KATEGORI == 'Basah':
+                basah += data.JUMLAH
+            if data.KATEGORI == 'Kering':
+                kering += data.JUMLAH
+        response.context_data['total'] = basah + kering
+        response.context_data['tanggal'] = tanggal
+        response.context_data['basah']=basah
+        response.context_data['kering']=kering
+
+        return response
+    
+admin.site.register(TabunganSampahProxy, TabunganSampahProxyAdmin)
