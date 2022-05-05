a546d235b368a88d60c954ba715138e91bca5ab7
blacidtt <maftuhpistol@gmail.com>
2022-05-05 12:25:09 +0700

    dummy sarpras

---
+++ adistetsa/sarana_prasarana/management/commands/setup_dummy_pre_sarana_prasarana.py
@@ -0,0 +1,37 @@
+from django.db import transaction
+from django.core.management.base import BaseCommand
+
+from sarana_prasarana.models import *
+
+
+class Command(BaseCommand):
+    help = "Melakukan generate data dummy dependensi sarana prasarana"
+
+    @transaction.atomic
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Menghapus data lama...")
+        models = [
+            JenisSarana, JenisRuangan
+        ]
+        for m in models:
+            m.objects.all().delete()
+
+        self.stdout.write("Membuat data baru...")
+
+        # Kategori Sarana
+        kategori_sarana = [
+            'Alat Tulis Kerja', 'Informasi Teknologi', 'Inventaris Rumah Tangga'
+        ]
+                
+        for jenis_sarana in kategori_sarana:
+            JenisSarana.objects.create(KATEGORI=jenis_sarana)
+        
+        # Kategori Ruangan    
+        kategori_ruangan = [
+            'Serbaguna', 'Kelas', 'Penunjang'
+        ]
+        
+        for jenis_ruangan in kategori_ruangan:
+            JenisRuangan.objects.create(KATEGORI=jenis_ruangan)
+        
+        
---
+++ adistetsa/sarana_prasarana/management/commands/setup_dummy_sarana_prasarana.py
@@ -0,0 +1,56 @@
+from django.db import transaction
+from django.core.management.base import BaseCommand
+
+from kurikulum.models import *
+from kurikulum.factories import *
+from sarana_prasarana.models import *
+
+import random
+
+class Command(BaseCommand):
+    help = "Melakukan generate data dummy sarana prasarana"
+
+    @transaction.atomic
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Menghapus data lama...")
+        models = [
+            Sarana, Ruangan
+        ]
+        for m in models:
+            m.objects.all().delete()
+
+        self.stdout.write("Membuat data baru...")
+        
+        # Sarana
+        sarana = [
+            {'NAMA':'LCD Proyektor','JENIS':JenisSarana.objects.get(KATEGORI='Informasi Teknologi'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Sapu','JENIS':JenisSarana.objects.get(KATEGORI='Inventaris Rumah Tangga'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Kabel HDMI','JENIS':JenisSarana.objects.get(KATEGORI='Informasi Teknologi'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Spidol','JENIS':JenisSarana.objects.get(KATEGORI='Alat Tulis Kerja'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Penghapus','JENIS':JenisSarana.objects.get(KATEGORI='Informasi Teknologi'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Bolpoin','JENIS':JenisSarana.objects.get(KATEGORI='Informasi Teknologi'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Kemoceng','JENIS':JenisSarana.objects.get(KATEGORI='Inventaris Rumah Tangga'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Cikrak','JENIS':JenisSarana.objects.get(KATEGORI='Inventaris Rumah Tangga'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Komputer','JENIS':JenisSarana.objects.get(KATEGORI='Informasi Teknologi'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Flashdisk','JENIS':JenisSarana.objects.get(KATEGORI='Informasi Teknologi'),'STATUS':'Sudah Dikembalikan'}
+        ]
+        
+        for data_sarana in sarana:
+            Sarana.objects.create(**data_sarana)
+        
+        # Ruangan
+        ruangan = [
+            {'NAMA':'A1','JENIS':JenisRuangan.objects.get(KATEGORI='Kelas'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'A2','JENIS':JenisRuangan.objects.get(KATEGORI='Kelas'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'A3','JENIS':JenisRuangan.objects.get(KATEGORI='Kelas'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'A4','JENIS':JenisRuangan.objects.get(KATEGORI='Kelas'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'A5','JENIS':JenisRuangan.objects.get(KATEGORI='Kelas'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Aula','JENIS':JenisRuangan.objects.get(KATEGORI='Serbaguna'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Lapangan','JENIS':JenisRuangan.objects.get(KATEGORI='Serbaguna'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Laboratorium Fisika','JENIS':JenisRuangan.objects.get(KATEGORI='Penunjang'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Laboratorium Kimia','JENIS':JenisRuangan.objects.get(KATEGORI='Penunjang'),'STATUS':'Sudah Dikembalikan'},
+            {'NAMA':'Laboratorium Komputer','JENIS':JenisRuangan.objects.get(KATEGORI='Penunjang'),'STATUS':'Sudah Dikembalikan'},
+        ]
+        
+        for data_ruangan in ruangan:
+            Ruangan.objects.create(**data_ruangan)
