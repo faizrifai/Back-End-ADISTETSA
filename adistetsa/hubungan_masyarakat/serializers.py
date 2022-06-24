c9c1a4adfafdcf2d0e520f2a5885fd0d06b3a688
arrazy100 <afdhalarrazy111@gmail.com>
2022-06-24 15:58:37 +0700

    Add tambah dan detail log uks karyawan, add daftar siswa, guru, dan karyawan

--- adistetsa/hubungan_masyarakat/serializers.py
+++ adistetsa/hubungan_masyarakat/serializers.py
@@ -1,6 +1,8 @@
 from .models import *
 from rest_framework import serializers
 
+from dataprofil.models import DataSiswa, DataGuru, DataKaryawan
+
 class LogUKSListSerializer(serializers.Serializer):
     ID = serializers.IntegerField()
     NAMA = serializers.CharField()
@@ -9,14 +11,34 @@ class LogUKSListSerializer(serializers.Serializer):
     DETAIL_URL = serializers.URLField()
 
 class LogUKSDetailSiswaSerializer(serializers.ModelSerializer):
+    NAMA = serializers.SerializerMethodField('get_nama')
+
     class Meta:
         model = LogUKSSiswa
         fields = '__all__'
 
+    def get_nama(self, obj):
+        return str(obj.NAMA)
+
 class LogUKSDetailTendikSerializer(serializers.ModelSerializer):
+    NAMA = serializers.SerializerMethodField('get_nama')
+
     class Meta:
         model = LogUKSTendik
         fields = '__all__'
+
+    def get_nama(self, obj):
+        return str(obj.NAMA)
+
+class LogUKSDetailKaryawanSerializer(serializers.ModelSerializer):
+    NAMA = serializers.SerializerMethodField('get_nama')
+
+    class Meta:
+        model = LogUKSKaryawan
+        fields = '__all__'
+
+    def get_nama(self, obj):
+        return str(obj.NAMA)
 
 class TambahLogUKSSiswaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -33,6 +55,11 @@ class TambahLogUKSTendikSerializer(serializers.ModelSerializer):
         model = LogUKSTendik
         fields = '__all__'
 
+class TambahLogUKSKaryawanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LogUKSKaryawan
+        fields = '__all__'
+
 class BukuTamuListSerializer(serializers.ModelSerializer):
     class Meta:
         model = BukuTamu
@@ -42,3 +69,27 @@ class BukuTamuPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BukuTamu
         exclude = ('HARI',)
+
+class DataSiswaSerializer(serializers.ModelSerializer):
+    NAMA = serializers.SerializerMethodField('get_nama')
+    KELAS = serializers.SerializerMethodField('get_kelas')
+
+    class Meta:
+        model = KelasSiswa
+        fields = '__all__'
+
+    def get_nama(self, obj):
+        return str(obj.NIS)
+
+    def get_kelas(self, obj):
+        return str(obj.KELAS)
+
+class DataGuruSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataGuru
+        fields = '__all__'
+
+class DataKaryawanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataKaryawan
+        fields = '__all__'
--- adistetsa/hubungan_masyarakat/urls.py
+++ adistetsa/hubungan_masyarakat/urls.py
@@ -6,8 +6,13 @@ urlpatterns = [
     path('hubungan_masyarakat/log_uks', LogUKSListView.as_view(), name='log_uks'),
     path('hubungan_masyarakat/detail_log_uks_siswa/<int:pk>', LogUKSDetailSiswaView.as_view(), name='detail_log_uks_siswa'),
     path('hubungan_masyarakat/detail_log_uks_tendik/<int:pk>', LogUKSDetailTendikView.as_view(), name='detail_log_uks_tendik'),
+    path('hubungan_masyarakat/detail_log_uks_karyawan/<int:pk>', LogUKSDetailKaryawanView.as_view(), name='detail_log_uks_karyawan'),
     path('hubungan_masyarakat/tambah_log_uks_siswa', TambahLogUKSSiswaView.as_view(), name='tambah_log_uks_siswa'),
     path('hubungan_masyarakat/tambah_log_uks_tendik', TambahLogUKSTendikView.as_view(), name='tambah_log_uks_tendik'),
+    path('hubungan_masyarakat/tambah_log_uks_karyawan', TambahLogUKSKaryawanView.as_view(), name='tambah_log_uks_karyawan'),
     path('hubungan_masyarakat/buku_tamu', BukuTamuListView.as_view(), name='buku_tamu'),
     path('hubungan_masyarakat/buku_tamu/<int:pk>', BukuTamuDetailView.as_view(), name='buku_tamu'),
+    path('hubungan_masyarakat/data_siswa', DataSiswaListView.as_view(), name='data_siswa_humas'),
+    path('hubungan_masyarakat/data_guru', DataGuruListView.as_view(), name='data_guru_humas'),
+    path('hubungan_masyarakat/data_karyawan', DataKaryawanListView.as_view(), name='data_karyawan_humas'),
 ]
